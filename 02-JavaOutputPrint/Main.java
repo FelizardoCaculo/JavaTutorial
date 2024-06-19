4cdf2ebf0818925866fc3833bc1827c6a5001e37
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-19 04:31:23 +0100

    Add 2º Lesson

---
+++ 02-JavaOutputPrint/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        //Java Output / Print
+        System.out.println("\nHello world!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        System.out.print("Hey! ");
+        System.out.print("I will print on the same line!");
+    }
+}
