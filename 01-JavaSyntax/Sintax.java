8e57ef24d4f73c2a0791d9d1857fd4b3ef57af01
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-19 20:34:28 +0100

    Refactor 1º Lesson

--- 01-JavaSyntax/Sintax.java
+++ 01-JavaSyntax/Sintax.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Sintax {
     public static void main(String[] args){
         System.out.println("\nOlá mundo!\n");
         System.out.println("Seja muito bem vindo ao curso Java!\n");
