9442d7b0ff3a06498f4f0e5b2d66eb3ff764985d
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-19 04:34:08 +0100

    Add 3º Lesson

---
+++ 03-JavaOutputNumbers/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args){
+        //Java Output Numbers
+        //Print Numbers
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        //Perform mathematical calculations
+        int soma = 3 + 3;
+        int multiplication = 3 * 3;
+        int subtration = 3 - 3;
+        float division = 3 / 3;
+        System.out.println("3 + 3 = " + soma);
+        System.out.println("3 x 3 = " + multiplication);
+        System.out.println("3 - 3 = " + subtration);
+        System.out.println("3 : 3 = " + division);
+
+    }
+}
