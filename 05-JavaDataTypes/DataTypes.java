578123d19fd8dec0db2f0f26d0332a2b7f53dde8
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-20 20:47:55 +0100

    Add 5º Lesson

---
+++ 05-JavaDataTypes/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args){
+        int myNumber = 5;
+        float myFloatNumber = 5.99f;
+        char myLetter = 'D';
+        boolean myBoolean = true;
+        String myText = "Olá";
+        System.out.println(myNumber);
+        System.out.println(myFloatNumber);
+        System.out.println(myLetter);
+        System.out.println(myBoolean);
+        System.out.println(myText);
+    }
+}
