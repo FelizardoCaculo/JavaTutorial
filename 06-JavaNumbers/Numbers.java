fc6efb307f595fffb270e286576b6e6bd150d2e5
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-21 19:05:42 +0100

    Add 6º Lesson

---
+++ 06-JavaNumbers/Numbers.java
@@ -0,0 +1,15 @@
+public class Numbers {
+    public static void main(String[] args){
+        /*
+         * A escolha do tipo primitivo depende do valor numérico
+         */
+        // para valores inteiros
+        byte idade = 36;
+        short ano = 2017;
+        int superficieAngola = 1246700;
+        long distanciaTerraLua = 21474836415622L;
+        // para valores de ponto flutuante
+        float peso = 76.5f;
+        double distanciaTerraNuvens = 12434655.25d;
+    }
+}
