d5a671e1650c361aacab33f17886956828acef3a
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-23 16:05:54 +0100

    Refactor 6º Lesson

--- 06-JavaNumbers/Numbers.java
+++ 06-JavaNumbers/Numbers.java
@@ -5,11 +5,18 @@ public class Numbers {
          */
         // para valores inteiros
         byte idade = 36;
-        short ano = 2017;
+        short ano = 1987;
         int superficieAngola = 1246700;
         long distanciaTerraLua = 21474836415622L;
         // para valores de ponto flutuante
         float peso = 76.5f;
         double distanciaTerraNuvens = 12434655.25d;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("Superfície de Angola: " + superficieAngola);
+        System.out.println("Distância da Terra para a Lua: " + distanciaTerraLua);
+        System.out.println("Peso: " + peso);
+        System.out.println("Distância da Terra para as nuvens: " + distanciaTerraNuvens);
     }
 }
