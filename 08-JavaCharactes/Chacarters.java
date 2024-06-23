fb96b3014cf6699eb4ba797a53da0a0993288691
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-23 17:11:41 +0100

    Add 8º Lesson

---
+++ 08-JavaCharactes/Chacarters.java
@@ -0,0 +1,17 @@
+public class Chacarters {
+    public static void main(String[] args){
+        char sex = 'M';
+        System.out.println("\nSou do sexo " + sex + "asculino");
+
+        //Can use ASCII values if you are familiar with them
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println("\nCaracteres ASCII\n");
+        System.out.println("65 = " + myVar1);
+        System.out.println("66 = " + myVar2);
+        System.out.println("67 = " + myVar3);
+
+        //Strings are sequence os characters
+        String theEnd = "Fim do algoritmo!";
+        System.out.println("\n" + theEnd);
+    }
+}
