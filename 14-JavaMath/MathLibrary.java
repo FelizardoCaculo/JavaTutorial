1fc107a5ee9ee56fd74e74337b78c85b7cbc223c
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-29 21:32:10 +0100

    14º Lesson CleanCode refactor

--- 14-JavaMath/MathLibrary.java
+++ 14-JavaMath/MathLibrary.java
@@ -6,20 +6,21 @@ public class MathLibrary {
         /*
          * A classe Math tem muitos métodos que permitem efectuar operações matemáticas em números
          */
-        Scanner input = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         System.out.println("=== Maior/Menor ===");
-        int x;
-        int y;
+        int firstNumber;
+        int lastNumber;
         int randomNumber = (int) (Math.random() * 101); //Fórmula para gerar um número aleatório entre 0 à 100
 
         System.out.println("Digite o primeiro número: ");
-        x = input.nextInt();
+        firstNumber = userInput.nextInt();
         System.out.println("Digite o segundo número: ");
-        y = input.nextInt();
+        lastNumber = userInput.nextInt();
         
         System.out.println(randomNumber + " ===> Este é um número aleatório gerado entre 0 à 100");
-        System.out.println("O maior número é: " + Math.max(x, y));
-        System.out.println("O menor número é: " + Math.min(x, y));
-        System.out.println("\nA raís quadrada de " + y + " é: " + Math.sqrt(y));
+        System.out.println("O maior número é: " + Math.max(firstNumber, lastNumber));
+        System.out.println("O menor número é: " + Math.min(firstNumber, lastNumber));
+        System.out.println("\nA raís quadrada de " + y + " é: " + Math.sqrt(lastNumber));
+        userInput.close();
     }
 }
