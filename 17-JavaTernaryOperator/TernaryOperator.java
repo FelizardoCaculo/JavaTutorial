6c5568510a012e198831fa7a586784bbda4bc798
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-29 04:43:23 +0100

    Refactor 17º Lesson code

--- 17-JavaTernaryOperator/TernaryOperator.java
+++ 17-JavaTernaryOperator/TernaryOperator.java
@@ -1,22 +1,24 @@
+import java.util.Scanner;
+
 public class TernaryOperator {
     public static void main(String[] args) {
-        /*
-         * Sintax
-         * variable = (condition) ? expressionTrue : expressionFalse;
-         */
+      /*
+      * Sintax
+      * variable = (condition) ? expressionTrue : expressionFalse;
+      */
+      Scanner input = new Scanner(System.in);
+      int time;
 
-         int time = 20;
-         
-         //if else statement
-         if (time < 18) {
-            System.out.println("Good day!");
-         } else {
-            System.out.println("Good evening!");
-         }
-        
-         //Ternary Operator
-         String result = (time < 18) ? "Good day!" : "Good evening!";
-         System.out.println(result);
-    }   
-    
+      System.out.println("\nQue horas são?");
+      time = input.nextInt();
+      //if else statement
+      if (time < 18) {
+         System.out.println("Good day!");
+      } else {
+         System.out.println("Good evening!");
+      }
+      //Ternary Operator
+      String result = (time < 18) ? "Good day!" : "Good evening!";
+      System.out.println(result);
+   }       
 }
