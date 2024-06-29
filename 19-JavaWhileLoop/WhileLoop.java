d34bb3f693514799ee5f3a2ab750d6777dfbf5f8
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-29 20:26:27 +0100

    19º Lesson cleanCode refactor

--- 19-JavaWhileLoop/WhileLoop.java
+++ 19-JavaWhileLoop/WhileLoop.java
@@ -1,23 +1,35 @@
+import java.util.Scanner;
+
 public class WhileLoop {
     public static void main(String[] args){
         //while loop
-        System.out.println("\nLoop com while");
-        int i = 0;
-        int j = 0;
-        while (i < 6){
-            System.out.println(i);
-            i++;
-        }
+        Scanner userInput = new Scanner(System.in);
+        int whileLoopCounter = 0;
+        int doWhileLoopCounter = 0;
+        int whileLoopControleVariable;
+        int doWhileLoopControleVariable;
 
-        while (i < 21){
-            System.out.println(i);
-            i++;
+        System.out.println("\nLoop com while");
+        System.out.println("\nEscreva um número inteiro para controlar o laço While: ");
+        whileLoopControleVariable = userInput.nextInt();
+        while (whileLoopCounter < whileLoopControleVariable){
+            System.out.println(whileLoopCounter);
+            whileLoopCounter++;
+        }
+        whileLoopCounter = 0;
+        System.out.println("\nEscreva outro númeri inteiro para controlar o segundo laço while: ");
+        whileLoopControleVariable = userInput.nextInt();
+        while (whileLoopCounter < whileLoopControleVariable){
+            System.out.println(whileLoopCounter);
+            whileLoopCounter++;
         }
         //do while loop
-        System.out.println("\nLoop com do while");
+        System.out.println("\nLoop com DoWhile");
+        doWhileLoopControleVariable = userInput.nextInt();
         do {
-            System.out.println(j);
-            j++;
-        } while (i < 10);
+            System.out.println(doWhileLoopCounter);
+            doWhileLoopCounter++;
+        } while (doWhileLoopCounter < doWhileLoopControleVariable);
+        userInput.close();
     }
 }
