9ae10f7341ea73ab8fadd2516415cc3572f29834
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-29 23:13:45 +0100

    12º Lesson CleanCode refactor

--- 12-JavaStringConcatenation/StringConcatenation.java
+++ 12-JavaStringConcatenation/StringConcatenation.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 public class StringConcatenation {
     public static void main(String[] args){
         //O sinal de adição + é usado para juntar Strings
-        String firstName = "Felizardo";
-        String lastName = "Caculo";
+        Scanner userInput = new Scanner(System.in);
+        String firstName;
+        String lastName;
 
+        System.out.println("\nEscreva seu primeiro nome: ");
+        firstName = userInput.next();
+        System.out.println("Escreva seu último nome: ");
+        lastName = userInput.next();
         System.out.println(firstName + lastName); //juntando strins sem espaço
         System.out.println(firstName + " " + lastName); //juntando strings com espaço
-
         //Também podemos usar o método abaixo para contenar
         System.out.println(firstName.concat(lastName));
+        userInput.close();
     }
 }
