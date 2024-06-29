df4888e2192b563ca2b189882fc1749b1fe343ea
FelizardoCaculo <felizardo1987@gmail.com>
2024-06-29 21:26:22 +0100

    15º Lesson CleanCode refactor

--- 15-JavaBoolean/BoleansDataType.java
+++ 15-JavaBoolean/BoleansDataType.java
@@ -2,27 +2,41 @@ import java.util.Scanner;
 
 public class BoleansDataType {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         boolean isJavaFun = true;
         boolean isFishTasty = false;
-        int x = 10;
-        int y = 20;
-        int minhaIdade;
-        int idadeParaVotar;
+        int firstNumber;
+        int lastNumber;
+        int myAge;
+        int minimuAgeForCitizenToVote;
+        boolean greaterThan;
+        boolean lessThan;
+        boolean equalTo;
 
+        System.out.println("\nEscreva o primeiro número: ");
+        firstNumber = userInput.nextInt();
+        System.out.println("Escreva o último número: ");
+        lastNumber = userInput.nextInt();
         System.out.println("Quantos anos voce tem? ");
-        minhaIdade = input.nextInt();
+        myAge = userInput.nextInt();
         System.out.println("Qual é a idade mínima para votar no teu país? ");
-        idadeParaVotar = input.nextInt();
+        minimuAgeForCitizenToVote = userInput.nextInt();
+
+        greaterThan = firstNumber > lastNumber;
+        lessThan = firstNumber < lastNumber;
+        equalTo = firstNumber == lastNumber;
+
         System.out.println(isJavaFun);
         System.out.println(isFishTasty);
-        System.out.println("\nO resultado da comparação (x > y) é: " + (x > y));
-        System.out.println("O resultado da comparação (x < y) é: " + (x < y));
-        System.out.println("O resultado da comparação (x == y) é: " + (x == y));
-        if (minhaIdade >= idadeParaVotar) {
+        System.out.println("\nO primeiro número digitado é maior? " + greaterThan);
+        System.out.println("O último número digitado é maior? " + lessThan);
+        System.out.println("O primeiro e o último número são iguais? " + equalTo);
+        if (myAge >= minimuAgeForCitizenToVote) {
             System.out.println("\nMinha idade me permite votar!");
         } else {
             System.out.println("\nMinha idade não me permite votar!");
         }
+        
+        userInput.close();
     }
 }
