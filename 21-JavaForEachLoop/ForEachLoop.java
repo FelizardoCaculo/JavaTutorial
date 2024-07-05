f14f55c54c2df69313827432003b4333dda1d5bb
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-05 04:01:08 +0100

    Add content to 21º Lesson

--- 21-JavaForEachLoop/ForEachLoop.java
+++ 21-JavaForEachLoop/ForEachLoop.java
@@ -5,11 +5,24 @@ public class ForEachLoop {
          */
 
         String[] carMarks = {"Volvo", "BMW", "Ford", "Mazda"};
+        byte[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 15, 16, 17, 18, 19, 20};
+        char[] theEnglishAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'};
         //For-Each structure in Java
-        System.out.println("\n");
+        System.out.print("\nLista de Marcas de carros: ");
         for (String iterableName : carMarks) {
-            System.out.println(iterableName);
+            System.out.print(iterableName.toUpperCase() + ", ");
         }
-        System.out.println("\n");
+
+        System.out.print("\n\nContagem numérica até 20: ");
+        for (byte iterableNumbers : numbers) {
+            System.out.print(iterableNumbers + ", ");
+        }
+
+        System.out.print("\n\nAlfabeto Inglês: ");
+        for (char iterableCharacters : theEnglishAlphabet) {
+            System.out.print(iterableCharacters + ", ");
+        }
+
+        System.out.println("\n\n");
     }
 }
