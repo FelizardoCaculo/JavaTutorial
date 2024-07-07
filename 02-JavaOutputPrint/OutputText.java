56cf55e94d6fe8affe7a5a6d51a80d8d4180d677
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-07 18:10:17 +0100

    Refactoring 2º Lesson code examples

--- 02-JavaOutputPrint/OutputText.java
+++ 02-JavaOutputPrint/OutputText.java
@@ -1,10 +1,24 @@
 public class OutputText{
     public static void main(String[] args){
         //Java Output / Print
-        System.out.println("\nHello world!");
+        System.out.println("\n===== Saída usando println =====");
+        System.out.println("Hello world!");
         System.out.println("I am learning Java.");
         System.out.println("It is awesome!");
+
+        System.out.println("\n===== Saída usando print =====");
         System.out.print("Hey! ");
         System.out.print("I will print on the same line!\n");
+
+        /*
+         *  %d => serve para representar dados inteiros
+         *  %s => serve para representar strings
+         *  %f => serve para representar floats
+         */
+        System.out.println("\n===== Saída usando printf =====");
+        System.out.printf("I am %d years old\n", 36);
+        System.out.printf("%s: %.2f kilos", "Meu peso é ", 76.2); //O .2 serve para delimitar o número de casas decimais depois da vírgula.
+
+        System.out.println();
     }
 }
