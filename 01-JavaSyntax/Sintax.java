cdbbec05f02533fdc8766986a36ce4c9894f1b75
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-14 11:59:23 +0100

    Add documentation and change Lesso 1º Lesson file name

--- 01-JavaSyntax/Sintax.java
+++
@@ -1,6 +0,0 @@
-public class Sintax {
-    public static void main(String[] args){
-        System.out.println("\nOlá mundo!\n");
-        System.out.println("Seja muito bem vindo ao curso Java!\n");
-    }
-}
