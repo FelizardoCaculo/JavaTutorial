52e84b86ce738b710036518a6bc8fd699dd4debd
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-15 21:22:22 +0100

    Add 23º Lesson new content

--- 23-JavaArrays/Arrays.java
+++ 23-JavaArrays/Arrays.java
@@ -1,9 +1,14 @@
 public class Arrays {
     public static void main(String[] args) {
         //Declaring an array
-        String[] carsMark;
+        String[] carsMark = new String[3];//o 3 represente o de elementos que o array vai conter
 
         //Assigning values to an array
+        carsMark[0] = "Fiat";
+        carsMark[1] = "Tesla";
+        carsMark[2] = "Ferrarri";
+
+        //Declaring and assigning values to an array
         String secondCarsMark[] = {"Volvo", "BMW", "Ford", "Mazda"};
 
         //Declaring and assigning values
@@ -18,13 +23,19 @@ public class Arrays {
         //Accessing the value of the first element
         System.out.println("\nO primogénito dos irmãos Caculo é o: " + caculoBrothers[0]);
 
+        //Printing an array
+        System.out.print("\n\nMarcas de Carro - Lista 1: ");
+        for(String iterableVariable : carsMark){
+            System.out.print(iterableVariable + ", ");
+        }
+
         //Changing an Array element
-        System.out.print("\n\nMarcas de Carro: ");
+        System.out.print("\n\nMarcas de Carro - Lista 2: ");
         for(String iterableVariable : secondCarsMark){
             System.out.print(iterableVariable + ", ");
         }
         secondCarsMark[0] = "Toyota";
-        System.out.print("\n\nMarcas de carro: ");
+        System.out.print("\n\nMarcas de carro - Lista 3: ");
         for(String iterableVariable : secondCarsMark){
             System.out.print(iterableVariable + ", ");
         }
