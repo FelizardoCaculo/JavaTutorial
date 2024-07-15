7ce0eb30c2d7f1c56a6c914eda5e5641864fa128
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-15 21:23:42 +0100

    Add 23º Lesson Exercise001

---
+++ 05-JavaDataTypes/Exercise001.java
@@ -0,0 +1,15 @@
+public class Exercise001 {
+    public static void main(String[] args) {
+        /*
+         * Crie variáveis para os campos descritos abaixo entre < > e imprima a seguinte mensagem.
+         * 
+         * Eu <nome>, morador do bairro <nome do baixo>, confirmo que recebi ao salário de <salário>, na data <data>.
+         */
+        String nome = "Felizardo";
+        String bairro = "4 de Fevereiro";
+        double salario = 250000;
+        String dataRecebimentoSalario = "25/06/2024";
+        String relatorio = "Eu " + nome + ", morador do bairro " + bairro + ", confirmo que recebi o salário de AKZ " + salario + ", na data " + dataRecebimentoSalario + ".";
+        System.out.println("\n" + relatorio + "\n");
+    }
+}
