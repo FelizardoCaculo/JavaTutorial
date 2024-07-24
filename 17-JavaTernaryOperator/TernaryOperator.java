bd7f3bf682105345de11d8e81e8b499a0cfbbd90
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-24 03:21:42 +0100

    Refactor 17º Lesson

--- 17-JavaTernaryOperator/TernaryOperator.java
+++ 17-JavaTernaryOperator/TernaryOperator.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
 public class TernaryOperator {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
       /*
       * Sintax
       * variable = (condition) ? expressionTrue : expressionFalse;
       */
       Scanner userInput = new Scanner(System.in);
-      int timeInHour;
+      int integerNumber;
 
-      System.out.println("\nQue horas são?");
-      timeInHour = userInput.nextInt();
-      //if else statement
-      if (timeInHour < 18) {
-         System.out.println("Good day!");
-      } else {
-         System.out.println("Good evening!");
-      }
+      System.out.println("\nEscreva um número inteiro qualquer: ");
+      integerNumber = userInput.nextInt();
+
       //Ternary Operator
-      String resultWithTernaryOperator = (timeInHour < 18) ? "Good day!" : "Good evening!";
+      String resultWithTernaryOperator = (integerNumber % 2 == 0) ? "O número digitado é PAR!" : "O número digitado é ÍMPAR!";
       System.out.println(resultWithTernaryOperator);
       userInput.close();
    }       
