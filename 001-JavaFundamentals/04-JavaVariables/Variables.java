0d43f4b43fc54f0d50c9bc905cba1d64869fc8f0
FelizardoCaculo <felizardo1987@gmail.com>
2024-08-30 03:01:27 +0100

    Refactor 001-JavaFundamentals/04-JavaVariables/Variables.java file

--- 001-JavaFundamentals/04-JavaVariables/Variables.java
+++ 001-JavaFundamentals/04-JavaVariables/Variables.java
@@ -1,4 +1,37 @@
 public class Variables{
+
+    /*
+     * Variable, is name of reserved area allocated in memory. n other words, it is a name of memory location. 
+     * Thera are 3 types of variables:
+     * 1 - Local Variable: it is declared inside the body of a method,
+     *      constructor or block, they can not be defined with "static"
+     *      keyword
+     * 2 - Instance Variable: it is declared inside the class, but 
+     *      outside of the body of the method, it is not declared as static.
+     *      Its value is instance specific and is not shared among instances.
+     * 3 - Static Variable: is declared as static (with static keyword). 
+     *      It cannot be local. You can create a single copy of static variable 
+     *      and share among all the instances of the class. Memory allocation
+     *      for static variable happens only once when the class is loaded in memory.
+     * 
+     *      Resumo
+     *      Local: Usada temporariamente dentro de métodos.
+     *      Instância: Pertence a objetos, cada instância tem sua própria cópia.
+     *      Estática: Compartilhada por todas as instâncias da classe.
+     */
+
+    // This is a "Local Variable:"
+    public void myMethod() {
+        int localVar = 10; // variável local
+        System.out.println(localVar);
+    }
+
+    // This is an "Instance Variable"
+    int instanceVar; // variável de instância
+
+    // This is an "Static Variable"
+    static int staticVar; // variável estática
+
     public static void main(String[] args){
         //Declaring variables ( type variableName = value )
         String name = "Felizardo";  //declaring and assigning a value
