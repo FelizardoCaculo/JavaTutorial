c1505e41d2c24e583bff138ae9924d546b62d366
FelizardoCaculo <felizardo1987@gmail.com>
2024-10-07 20:20:02 +0100

    Add 001-JavaFundamentals/20-JavaForLoop/Ex003TabuadaGeral.java

--- 001-JavaFundamentals/20-JavaForLoop/Ex003TabuadaGeral.java
+++ 001-JavaFundamentals/20-JavaForLoop/Ex003TabuadaGeral.java
@@ -1,13 +1,16 @@
-public class TabuadaGeral {
+public class Ex003TabuadaGeral {
     public static void main(String[] args) {
-        //Exemple using nested loop   
+
+        // Exemple using nested loop
         System.out.println("\n=== Tabuada Geral de 1 à 12 ===");
-        for(int counterVariable = 1; counterVariable <= 12; counterVariable++){
+        for (int counterVariable = 1; counterVariable <= 12; counterVariable++) {
             System.out.println("\nTabuada de: " + counterVariable + "\n");
-            for(int tabuada = 1; tabuada <=12; tabuada++){
+            for (int tabuada = 1; tabuada <= 12; tabuada++) {
                 System.out.println(counterVariable + " X " + tabuada + " = " + counterVariable * tabuada);
             }
             System.out.println("=============");
         }
+
     }
+
 }
