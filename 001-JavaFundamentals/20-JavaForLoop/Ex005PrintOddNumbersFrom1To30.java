ecdfe085e76b711e89ada923782966292d7d57f6
FelizardoCaculo <felizardo1987@gmail.com>
2024-10-07 21:24:38 +0100

    Add 001-JavaFundamentals/20-JavaForLoop/Ex005PrintOddNumbersFrom1To30.java

---
+++ 001-JavaFundamentals/20-JavaForLoop/Ex005PrintOddNumbersFrom1To30.java
@@ -0,0 +1,12 @@
+public class Ex005PrintOddNumbersFrom1To30 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("\nImprimir números ímpares de 1 a 30: \n");
+        for (int controlVariable = 1; controlVariable <= 30; controlVariable+= 2) {
+            System.out.println(controlVariable);
+        }
+
+    }
+
+}
