b850176417d11cae990fcdb755581d2442ceda7d
FelizardoCaculo <felizardo1987@gmail.com>
2024-10-07 20:11:35 +0100

    Add 001-JavaFundamentals/20-JavaForLoop/Ex002PrintEvenNumbersFrom1To20.java

---
+++ 001-JavaFundamentals/20-JavaForLoop/Ex001Printn10Numbers.java
@@ -0,0 +1,12 @@
+public class Ex001Printn10Numbers {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nContando de 1 à 10: \n");
+        for (int controlVariable = 1; controlVariable <= 10; controlVariable++) {
+            System.out.println(controlVariable);
+        }
+
+    }
+
+}
---
+++ 001-JavaFundamentals/20-JavaForLoop/Ex002PrintEvenNumbersFrom1To20.java
@@ -0,0 +1,11 @@
+public class Ex002PrintEvenNumbersFrom1To20 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("\nImprimir números pares de 1 a 20: \n");
+        for (int controlVarriable = 2; controlVarriable <= 20; controlVarriable += 2){
+            System.out.println(controlVarriable);
+        }
+
+    }
+}
