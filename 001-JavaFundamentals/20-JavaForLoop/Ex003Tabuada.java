b4bc20cb60447e97e53e6b56a8c01b3221aedbe9
FelizardoCaculo <felizardo1987@gmail.com>
2024-10-07 20:18:26 +0100

    Update 001-JavaFundamentals/20-JavaForLoop/Ex003Tabuada.java

--- 001-JavaFundamentals/20-JavaForLoop/Ex003Tabuada.java
+++ 001-JavaFundamentals/20-JavaForLoop/Ex003Tabuada.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ex003Tabuada {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
         System.out.println("\nEscreva um número inteiro para calcular sua tabuada: ");
@@ -10,12 +10,12 @@ public class Ex003Tabuada {
         System.out.println("\nTabuada de: " + tabuada + "\n");
         System.out.println("=============\n");
 
-        for(int counterVariable = 1; counterVariable <= 12; counterVariable++){
+        for (int counterVariable = 1; counterVariable <= 12; counterVariable++) {
             System.out.println(counterVariable + " X " + tabuada + " = " + counterVariable * tabuada);
         }
         System.out.println("\n=============\n");
         userInput.close();
-        
+
     }
 
 }
