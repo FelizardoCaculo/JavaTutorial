dd952d1aa561ade96b999e983057c4fc7013a24e
FelizardoCaculo <felizardo1987@gmail.com>
2024-10-07 19:44:35 +0100

    add 001-JavaFundamentals/20-JavaForLoop/ex001Printn10Numbers.java

---
+++ 001-JavaFundamentals/20-JavaForLoop/ex001Printn10Numbers.java
@@ -0,0 +1,11 @@
+public class ex001Printn10Numbers {
+
+    public static void main(String[] args) {
+        
+        for (int controlVariable = 1; controlVariable <= 10; controlVariable++){
+            System.out.println(controlVariable);
+        }
+
+    }
+
+}
