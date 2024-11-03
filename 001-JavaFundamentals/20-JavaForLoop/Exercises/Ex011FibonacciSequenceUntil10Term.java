6842cebe18703fbc3f52bb0075ddeba608b6e159
FelizardoCaculo <felizardo1987@gmail.com>
2024-11-03 16:20:15 +0100

    Add 001-JavaFundamentals/20-JavaForLoop/Exercises/Ex011FibonacciSequenceUntil10Term.java

--- 001-JavaFundamentals/20-JavaForLoop/Exercises/Ex011FibonacciSequenceUntil10Term.java
+++ 001-JavaFundamentals/20-JavaForLoop/Exercises/Ex011FibonacciSequenceUntil10Term.java
@@ -3,12 +3,12 @@ package Exercises;
 public class Ex011FibonacciSequenceUntil10Term {
     public static void main(String[] args) {
         int firstNumber = 0, secondNumber = 1;
-        System.out.println(firstNumber);
-        System.out.println(secondNumber);
-        System.out.println("======Sequência de Fibonacci======");
+        System.out.println("\n=======Sequência de Fibonacci========\n");
+        System.out.println("1º Termo da sequência de Fibonacci: " + firstNumber);
+        System.out.println("2º Termo da sequência de Fibonacci: " + secondNumber);
         for (int i = 3; i <= 10; i++) {
             int fibonacci = firstNumber + secondNumber;
-            System.out.println(fibonacci);
+            System.out.println(i + "º Termo da sequência de Fibonacci: " + fibonacci);
             firstNumber = secondNumber;
             secondNumber = fibonacci;
         }
